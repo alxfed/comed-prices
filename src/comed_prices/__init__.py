29db7f366aec3e2fe699a8ef99cb9994b466f26e
Alex Fedotov <alxfed@gmail.com>
2025-10-19 18:52:27 -0500

    first version

--- src/comed_prices/__init__.py
+++ src/comed_prices/__init__.py
@@ -5,3 +5,17 @@
 This source code is licensed under the license found in the
 LICENSE file in the root directory of this source tree.
 """
+from .read import (
+    current_hour_average_price,
+    five_minute_prices)
+from .utils import (
+    millis_to_local_time
+)
+
+__all__ = [
+    'current_hour_average_price',
+    'five_minute_prices',
+    'millis_to_local_time'
+]
+
+__version__ = '0.0.3'
